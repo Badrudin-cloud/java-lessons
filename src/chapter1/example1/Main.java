7f87762145d8d6164a92a08bd2c75042cff7fe0a
Badrudin Mohamed Ali <badrudiin@yooltech.com>
2023-03-02 22:57:13 +0300

    First commit

---
+++ src/chapter1/example1/Main.java
@@ -0,0 +1,8 @@
+package chapter1.example1;
+
+public class Main {
+    // This program print Welcome to Java
+    public static void main(String[] args){
+        System.out.println("Welcome to Java!");
+    }
+}
---
+++ src/chapter1/example2/ComputeExpression.java
@@ -0,0 +1,9 @@
+package chapter1.example2;
+
+public class ComputeExpression {
+    public static void main(String[] args) {
+        System.out.print("(10.5 + 2 * 3)/(45 - 3.5) = ");
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+
+    }
+}
